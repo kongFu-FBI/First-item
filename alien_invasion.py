38b33602afbead299296a2eed14b03afafa48787
kongFu-FBI <shifulong7@gmail.com>
2021-04-26 13:12:52 +0800

    d

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,3 +4,17 @@ class AlienInvasion:
     """管理游戏资源和行为的类"""
     def __init__(self):
         pygame.init()
+        self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption("Alien Invasion")
+
+    def ren_game(self):
+        while True:
+            """开始游戏的主循环"""
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            pygame.display.flip()
+if __name__ == '__main__':
+    #创建游戏实例并运行游戏
+    ai = AlienInvasion()
+    ai.ren_game()
