9290d4b3de1290acb4149f45c9e130d2e1ebd4d2
kongFu-FBI <shifulong7@gmail.com>
2021-04-29 19:04:32 +0800

    12.7

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -11,15 +12,45 @@ class AlienInvasion:
         self.settings = Settings()  # 创造了Settings实例并赋值self.settings
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)  # 传入当前的self
 
     def ren_game(self):
         while True:
+            self._check_events()
+            self.ship.update()
             """开始游戏的主循环"""
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            self.screen.fill(self.settings.bg_color)
-            pygame.display.flip()
+            self._update_screen()
+
+    def _check_events(self):
+        """响应按键和鼠标和事件"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _update_screen(self):
+        """更新屏幕上的图像，并切换到新屏幕"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()  # 使得飞机图像出现在屏幕上
+        pygame.display.flip()
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:  # 检查是否按下键(event.key)  是否是右箭头(pygame.K_RIGHT)
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:  # 按    Q退出
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+            #  向右移动飞船
 
 
 if __name__ == '__main__':
