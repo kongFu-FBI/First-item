53c2f41023718822b02d606b3f2a9049f410777f
kongFu-FBI <shifulong7@gmail.com>
2021-04-29 20:12:56 +0800

    12.7

--- settings.py
+++ settings.py
@@ -7,3 +7,9 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
         self.ship_speed = 1.5
+
+        # 子弹设置 宽3像素 高15像素 深灰色子弹
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
